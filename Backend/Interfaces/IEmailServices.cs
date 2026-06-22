1036a4f1f9bc244fa264002c8dff7a8421865825
vijithaputluru <vijithaputluru2003@gmail.com>
2026-06-22 16:21:08 +0530

    Update IEmailServices.cs

--- Backend/Interfaces/IEmailServices.cs
+++ Backend/Interfaces/IEmailServices.cs
@@ -1,21 +1,38 @@
 namespace EmployeeManagementSystem.Interfaces
+
 {
+
     public interface IEmailService
+
     {
+
         void SendOtp(string toEmail, string otp);
-        void SendEmployeeCredentials(string toEmail, string employeeName);
+ 
+        void SendEmployeeCredentials(
 
-        Task SendEmailWithAttachment(
             string toEmail,
+
+            string employeeName);
+ 
+        Task SendEmailWithAttachment(
+
+            string toEmail,
+
             string subject,
+
             string body,
+
             string attachmentPath);
-
+ 
         Task SendEmailAsync(
-    string toEmail,
-    string subject,
-    string body);
-     
+
+            string toEmail,
+
+            string subject,
+
+            string body);
+
+    }
 
 }
-}
+ 
